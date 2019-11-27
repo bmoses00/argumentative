b632fbff74a87fc3ee556ad9c637dfdd183bb44c
bmoses00 <bmoses00@stuy.edu>
2019-11-26 19:06:16 -0500

    Added work

--- parse_args.c
+++ parse_args.c
@@ -2,14 +2,22 @@
 # include <string.h>
 # include <stdio.h>
 
+char ** parse_args( char * line ) {
+	line = calloc(100, 1);
+	char * curr = line;
+	char * args[8];	
+	int i;
+	for (i = 0; curr != NULL; i++) {
+		args[i] = strsep(&curr, " ");
+	}
+	return pointer to original thing
+}
+
+
 int main(int argc, char * argv[]) {
-  char line[100] = "ls -a -l";
-  char * curr = line;
-  char * token;
-  char * args[8];
-  int i;
-  for (i = 0; curr != NULL; i++) {
-    args[i] = strsep(&curr, " ");
-  }
+  char ** line = parse_args("ls -a -l");
+  
   execvp(args[0], args);
 }
+
+
